18c4d2b92550945bbab0e9773574afd0ef0ae2c6
Lasindu Ashan <lasinduashan99@gmail.com>
2021-01-27 11:32:26 +0530

    new

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ func main() {
 
 	fmt.Print("Enter LKR Amount : ")
 	fmt.Scanf("%f", &usdAmount)
-	fmt.Printf("LKR Amount : %f  \n", usdAmount * 0.0051)
+	fmt.Printf("USD Amount : %f  \n", usdAmount*0.0051)
 
 	fmt.Print("Enter USD Amount : ")
 	fmt.Scanf("%f", &lkrRuppes)
-	fmt.Printf("USD Amount : %f  \n", lkrRuppes * 187.99)
+	fmt.Printf("LKR Amount : %f  \n", lkrRuppes*187.99)
 }
